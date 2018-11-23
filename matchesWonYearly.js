008ee9feb1d8ed84c1f6c46c352af0b4da1d5a40
vikas <vrsahu29@gmail.com>
2018-11-23 07:45:30 -0800

    List of Matches Won per Year

--- matchesWonYearly.js
+++ matchesWonYearly.js
@@ -1,32 +1,33 @@
 let csvToJson = require('convert-csv-to-json');
-let iplData=csvToJson.fieldDelimiter(',') .getJsonFromCsv('matches.csv');
-let returnWins={};
+let iplData = csvToJson.fieldDelimiter(',').getJsonFromCsv('matches.csv');
+let returnWins = {};
 
+function matchesWonYearly(data) {
+    let obj = {};
+    let count = 1;
+    for (let i = 0; i < data.length - 1; i++) {
+        if (data[i].season == data[i + 1].season) {
+            if (obj.hasOwnProperty(data[i].winner)) {
+                obj[data[i].winner]++;
+            } else {
+                obj[data[i].winner] = count;
+            }
+        } else {
+            if (obj.hasOwnProperty(data[i].winner)) {
+                obj[data[i].winner]++;
+            } else {
+                obj[data[i].winner] = count;
+            }
+            returnWins[data[i].season] = obj;
+            count = 1;
+            obj = {};
+        }
+    }
 
-function matchesWonYearly(data){
-let obj={};
-let count=1;
-for(let i=0;i<data.length-1;i++){
-	if(data[i].season==data[i+1].season){
-		if(obj.hasOwnProperty(data[i].winner)){
-			obj[data[i].winner]++;
-		}else{
-			obj[data[i].winner]=count;
-		}
-	}else{
-		if(obj.hasOwnProperty(data[i].winner)){
-			obj[data[i].winner]++;
-		}else{
-			obj[data[i].winner]=count;
-		}
-		returnWins[data[i].season]=obj;
-		count=1;
-		obj={};
-	}
+    return returnWins;
 }
 
-console.log(returnWins);	
-}	
 
-
-matchesWonYearly(iplData);
+module.exports = {
+    matchesWonYearly: matchesWonYearly
+}
