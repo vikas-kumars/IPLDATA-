314dd654d515dcf15c3bbf235622786362ec23d6
vikas <vrsahu29@gmail.com>
2018-11-23 07:46:40 -0800

    top economical bolwer

--- economical.js
+++ economical.js
@@ -1,84 +1,53 @@
-var fs = require('fs'); 
-var matches = fs.readFileSync('matches.csv','utf8');
-var deliveries=fs.readFileSync('deliveries.csv','utf8');
+const csv = require("fast-csv");
 
-var matchesdata=[];
-let detail=matches.split('\n');
-for(let i=0;i<detail.length;i++){
-  if(detail[i].includes(2015)){
-    matchesdata.push(detail[i]);
-  }
+function getEconomicalBowlers(deliveriesFile, matchIDArray){
+    return new Promise(function(resolve, reject){
+        let runsEachBowler = {}
+        let ballsBowled = {};
+        let bowlerEconomy = {};
+        let economicBowlers = {};
+        csv.fromPath(deliveriesFile)
+                .on("data", function(delivery){
+                    let matchID = parseInt(delivery[0]);
+                    let bowler = delivery[8];
+                    let wideBallRun = parseInt(delivery[10]);
+                    let noBallRun = parseInt(delivery[13]);
+                    let batsmanRun = parseInt(delivery[15]);
+
+
+                    if(matchIDArray.includes(matchID)){
+                        if(!runsEachBowler[bowler]){
+                            runsEachBowler[bowler] = wideBallRun + noBallRun + batsmanRun;
+                            if(!wideBallRun && !noBallRun){
+                                ballsBowled[bowler] = 1;
+                            }
+                        }else{
+                            runsEachBowler[bowler] += wideBallRun + noBallRun + batsmanRun;
+                            if(!wideBallRun && !noBallRun){
+                                if(ballsBowled[bowler]){
+                                    ballsBowled[bowler]++;
+                                }else{
+                                    ballsBowled[bowler] = 1;
+                                }
+                            }
+                        }
+                    }
+                })
+                .on("end", function(){
+                    for(let key in runsEachBowler){
+                        bowlerEconomy[key] = parseFloat((runsEachBowler[key]/ballsBowled[key]*6).toFixed(2));
+                    }
+                    let sortedArray
+                    sortedArray = Object.keys(bowlerEconomy).sort(function(a,b){return bowlerEconomy[a] - bowlerEconomy[b]});
+                    sortedArray.map(function(key, index){
+                        if(index > 9){return true;}
+                        economicBowlers[key] = bowlerEconomy[key];
+                    })
+                    resolve(economicBowlers);
+                })
+    })
 }
 
-var uniqueID=[];
-for(let id in matchesdata){
-  let line=matchesdata[id].split(',');
-  uniqueID.push(line[0]);
+module.exports = {
+    getEconomicalBowlers: getEconomicalBowlers
 }
-
-
-let eachLine=deliveries.split("\n");
-let delvData=[];
-for ( let i=0;i<eachLine.length;i++){
-  let splitEachLine=eachLine[i].split(",");
-  let x=parseInt(splitEachLine[0]); 
-  if(x>=parseInt(uniqueID[0])&& x<=parseInt(uniqueID[uniqueID.length-1])){
-    delvData.push(eachLine[i]);}
-}
-
-let splitDelv=[];
-for(let i=0;i<delvData.length;i++){
-   splitDelv[i]=delvData[i].split(",");
-}
-//console.log(splitDelv);
-
-let obj1={};
-
-for(let i=0;i<uniqueID.length;i++){
-
-  for(j=0;j<splitDelv.length;j++){
-    if(uniqueID[i]===splitDelv[j][0]){
-      if(obj1.hasOwnProperty(splitDelv[j][8])){
-        obj1[splitDelv[j][8]]=obj1[splitDelv[j][8]]+parseInt(splitDelv[j][17]); 
-
-      }else{
-        obj1[splitDelv[j][8]]=parseInt(splitDelv[j][17]);   
-      }
-      }
-  }
-}
-
-let obj2={};
-count=1;
-
-
-  for(j=0;j<splitDelv.length-1;j++){
-
-    	if(splitDelv[j][8]===splitDelv[j+1][8]){
-    	      if(obj2.hasOwnProperty(splitDelv[j][8])){
-    	        obj2[splitDelv[j][8]]++; 
-    	
-    	      }else{
-    	        obj2[splitDelv[j][8]]=count;  
-    	     
-    	      }}
-    	      else{
-    	      		  if(obj2.hasOwnProperty(splitDelv[j][8])){
-    	        obj2[splitDelv[j][8]]++; 
-    	
-    	      }else{
-    	        obj2[splitDelv[j][8]]=count;  
-    	        
-    	      }
-    	      }
-      }
-  
-
- 
-
-console.log(obj1);
-console.log(obj2);
-//console.log(obj);
-
-
-
