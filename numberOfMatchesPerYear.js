095dad8c759a66f708bf263839231f02fafdaf4c
vikas <vrsahu29@gmail.com>
2018-11-22 21:36:22 -0800

    updating number of matches per year

--- numberOfMatchesPerYear.js
+++ numberOfMatchesPerYear.js
@@ -1,38 +1,22 @@
-let csvToJson = require('convert-csv-to-json');
-let iplData=csvToJson.fieldDelimiter(',') .getJsonFromCsv('matches.csv');
-//console.log(iplData);
-let seasonArr=[];
-	for(let i=0;i<iplData.length;i++){
-		seasonArr.push(iplData[i].season);
-	}
-	seasonArr.sort();
-let returnMatches={};
-//------------------------------------------------------------------------------//
-function matchPerYear(data){
-var current = null;
-    var cnt = 0;
-    for (var i = 0; i < seasonArr.length; i++) {
-        if (seasonArr[i] != current) {
-            if (cnt > 0) {
-               // console.log(current + ' comes --> ' + cnt + ' times');
-                returnMatches[current]=cnt;
-            }
-            current = seasonArr[i];
-            cnt = 1;
-        } else {
-            cnt++;
-        }
-    }
-    if (cnt > 0) {
-        //console.log(current + ' comes --> ' + cnt + ' times');
-        returnMatches[current]=cnt;
-    }
-    console.log(returnMatches);
+const csv = require("fast-csv");
+
+function getMatchesPerYear(matchesFile){
+    return new Promise(function(resolve, reject){
+        let counter = 0;
+        let matchesPerSeason = {};
+        csv.fromPath(matchesFile)
+                .on("data", function(match){
+                    let season = match[1];
+                    if(counter){
+                        matchesPerSeason[season] = !matchesPerSeason.hasOwnProperty(season) ? 1 : matchesPerSeason[season] + 1;
+                    }
+                    counter++;
+                })
+                .on("end", function(){
+                   resolve(matchesPerSeason);
+                });
+    })
+    //console.log(matchesPerSeason);
 }
-//<---------------------------------------------------------------->
 
 
-//------------------------------------------------------------------------
-
-
-matchPerYear(seasonArr);
