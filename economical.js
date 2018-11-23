90b99e7a77183cadf574cc6879d46b226d4e6d7b
vikas <vrsahu29@gmail.com>
2018-11-23 09:18:34 -0800

    Improvement in the economical

--- economical.js
+++ economical.js
@@ -1,50 +1,58 @@
-const csv = require("fast-csv");
+let csv = require("fast-csv");
 
-function getEconomicalBowlers(deliveriesFile, matchIDArray){
-    return new Promise(function(resolve, reject){
+function getEconomicalBowlers(deliveriesFile, matchIDArray) {
+    return new Promise(function(resolve, reject) {
         let runsEachBowler = {}
         let ballsBowled = {};
         let bowlerEconomy = {};
         let economicBowlers = {};
         csv.fromPath(deliveriesFile)
-                .on("data", function(delivery){
-                    let matchID = parseInt(delivery[0]);
-                    let bowler = delivery[8];
-                    let wideBallRun = parseInt(delivery[10]);
-                    let noBallRun = parseInt(delivery[13]);
-                    let batsmanRun = parseInt(delivery[15]);
+            .on("data", function(delivery) {
+                let matchID = parseInt(delivery[0]);
+                let bowler = delivery[8];
+                let wideBallRun = parseInt(delivery[10]);
+                let noBallRun = parseInt(delivery[13]);
+                let batsmanRun = parseInt(delivery[15]);
+                let totalRun = parseInt(delivery[17]);
+                let byeRun = parseInt(delivery[11]);
+                let legbye = parseInt(delivery[12]);
 
 
-                    if(matchIDArray.includes(matchID)){
-                        if(!runsEachBowler[bowler]){
-                            runsEachBowler[bowler] = wideBallRun + noBallRun + batsmanRun;
-                            if(!wideBallRun && !noBallRun){
+
+                if (matchIDArray.includes(matchID)) {
+                    if (!runsEachBowler[bowler]) {
+                        runsEachBowler[bowler] = totalRun - byeRun - legbye; 
+                        if (!wideBallRun && !noBallRun) {
+                            ballsBowled[bowler] = 1;
+                        }
+                    } else {
+                        runsEachBowler[bowler] += totalRun - byeRun - legbye; 
+                        if (!wideBallRun && !noBallRun) {
+                            if (ballsBowled[bowler]) {
+                                ballsBowled[bowler]++;
+                            } else {
                                 ballsBowled[bowler] = 1;
-                            }
-                        }else{
-                            runsEachBowler[bowler] += wideBallRun + noBallRun + batsmanRun;
-                            if(!wideBallRun && !noBallRun){
-                                if(ballsBowled[bowler]){
-                                    ballsBowled[bowler]++;
-                                }else{
-                                    ballsBowled[bowler] = 1;
-                                }
                             }
                         }
                     }
-                })
-                .on("end", function(){
-                    for(let key in runsEachBowler){
-                        bowlerEconomy[key] = parseFloat((runsEachBowler[key]/ballsBowled[key]*6).toFixed(2));
+                }
+            })
+            .on("end", function() {
+                for (let key in runsEachBowler) {
+                    bowlerEconomy[key] = parseFloat((runsEachBowler[key] / ballsBowled[key] * 6).toFixed(2));
+                }
+                let sortedArray
+                sortedArray = Object.keys(bowlerEconomy).sort(function(a, b) {
+                    return bowlerEconomy[a] - bowlerEconomy[b]
+                });
+                sortedArray.map(function(key, index) {
+                    if (index > 9) {
+                        return true;
                     }
-                    let sortedArray
-                    sortedArray = Object.keys(bowlerEconomy).sort(function(a,b){return bowlerEconomy[a] - bowlerEconomy[b]});
-                    sortedArray.map(function(key, index){
-                        if(index > 9){return true;}
-                        economicBowlers[key] = bowlerEconomy[key];
-                    })
-                    resolve(economicBowlers);
+                    economicBowlers[key] = bowlerEconomy[key];
                 })
+                resolve(economicBowlers);
+            })
     })
 }
 
